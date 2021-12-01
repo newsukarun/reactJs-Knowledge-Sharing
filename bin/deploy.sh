f20ddbb71db510ee1abd04b8d268568b0aa1cf7a
Arun Chaitanya jami <arun.chaitanya@ntsindia.co.in>
2021-12-01 13:44:25 +0530

    Ci to build git pages

---
+++ bin/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -ex
+
+# Bundle the docs to html and copy to the docs/ folder
+npm run build
+
+# Copy all the yaml files to the docs/ folder
+mv build docs/
+
+# Ensure we have git appropriately configured to be able to commit & push
+git config --global user.name "$( git log --format=%an -n 1 ${COMMIT_SHA} )"
+git config --global user.email "$( git log --format=%ae -n 1 ${COMMIT_SHA} )"
+
+# Deploy
+if [ -z "$CIRCLE_TAG" ]; then
+  # Deploy latest
+  npx gh-pages-multi deploy --template index.html;
+else
+  # Deploy specific tagged version
+  npx gh-pages-multi deploy --template index.html --better-target --target $CIRCLE_TAG;
+fi
--- src/ClassApp.js
+++ src/ClassApp.js
@@ -1,9 +1,35 @@
-import { Component } from "react";
+import {Component} from "react";
 
 class ClassApp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        //clearInterval(this.timerID);
+    }
 
     render() {
-        return <h1>Hello, Arun</h1>;
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
     }
 }
 
