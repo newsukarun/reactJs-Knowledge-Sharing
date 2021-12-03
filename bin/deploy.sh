629019b29b2439501c5151b6b7bdbb25a380ca68
Arun Chaitanya jami <arun.chaitanya@ntsindia.co.in>
2021-12-03 14:53:18 +0530

    circle ci config file

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -23,5 +23,3 @@ else
   npx gh-pages-multi deploy --template index.pug --target $CIRCLE_TAG;
   npx gh-pages-multi deploy --template index.pug --target "async/${CIRCLE_TAG}";
 fi
-
-pug --pretty --extension html --doctype html --client  "async/index.pug"
