c5ec21f4b420d7202e8c1f67cb7843c0db0d62e2
Arun Chaitanya jami <arun.chaitanya@ntsindia.co.in>
2021-12-03 10:21:58 +0530

    circle ci config file

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,11 +4,6 @@ set -ex
 # Bundle the docs to html and copy to the docs/ folder
 npm run build
 
-mkdir -p docs
-
-# Copy all the yaml files to the docs/ folder
-mv build docs/
-
 # Ensure we have git appropriately configured to be able to commit & push
 git config --global user.name "$( git log --format=%an -n 1 ${COMMIT_SHA} )"
 git config --global user.email "$( git log --format=%ae -n 1 ${COMMIT_SHA} )"
@@ -17,6 +12,7 @@ git config --global user.email "$( git log --format=%ae -n 1 ${COMMIT_SHA} )"
 if [ -z "$CIRCLE_TAG" ]; then
   # Deploy latest
   npx gh-pages-multi deploy --template index.pug;
+  npx gh-pages-multi deploy --template index.pug --target "/async";
 else
   # Deploy specific tagged version
   npx gh-pages-multi deploy --template index.pug --target $CIRCLE_TAG;
